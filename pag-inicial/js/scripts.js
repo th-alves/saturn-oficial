437a02c1f955489ae412f333f93d6837f844ed9d
Thiago Alves de Souza <thiagosouza.contato01@gmail.com>
2023-09-27 20:20:51 -0300

    att

--- pag-inicial/js/scripts.js
+++ pag-inicial/js/scripts.js
@@ -1,7 +1,52 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const modal = document.getElementById('modal');
+const loginTab = document.getElementById('loginTab');
+const signupTab = document.getElementById('signupTab');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+const body = document.body;
+
+openModalBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+    loginTab.classList.add('active');
+    signupTab.classList.remove('active');
+    loginForm.style.display = 'block';
+    signupForm.style.display = 'none';
+    body.classList.add('open');
+    
+    // Ocultar o botão "Abrir Modal" quando o modal é aberto
+    openModalBtn.style.display = 'none';
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    body.classList.remove('open');
+    
+    // Mostrar o botão "Abrir Modal" quando o modal é fechado
+    openModalBtn.style.display = 'block';
+});
+
+loginTab.addEventListener('click', () => {
+    loginTab.classList.add('active');
+    signupTab.classList.remove('active');
+    loginForm.style.display = 'block';
+    signupForm.style.display = 'none';
+});
+
+signupTab.addEventListener('click', () => {
+    signupTab.classList.add('active');
+    loginTab.classList.remove('active');
+    signupForm.style.display = 'block';
+    loginForm.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+        body.classList.remove('open');
+        openModalBtn.style.display = 'block';
+    }
+});
+
+modal.style.display = 'none';
